571a45aea667f915d631b0088b2a702f1dbc29e7
fullzer4 <gabrielpelizzaro@gmail.com>
2026-06-08 16:16:13 -0300

    template: atualiza para Prisma 7 com better-sqlite3 adapter

--- template/backend/src/db.js
+++ template/backend/src/db.js
@@ -1,5 +1,7 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaClient } from '../generated/prisma/client.js'
 
-const prisma = new PrismaClient()
+const adapter = new PrismaBetterSqlite3({ url: 'file:../data.db' })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
